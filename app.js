a084b635910a7a77aae7fae2ed603bfa849ea0ae
Christine Le <christinele.cs@gmail.com>
2016-09-10 16:33:09 -0700

    add json file parsing

--- app.js
+++ app.js
@@ -5,18 +5,46 @@ var express = require('express')
 app.use(express.static(__dirname + '/public'))
 
 var artfile = './public/data/art.json';
-console.log('********** artfile *********************', artfile);
-var json;
+var dancefile = './public/data/dance.json';
+var musicfile = './public/data/music.json';
+var theatrefile = './public/data/theatre.json';
+
+var artObj = '';
+var danceObj = '';
+var musicObj = '';
+var theatreObj = '';
 
 fs.readFile(artfile, 'utf8', function(err, data){
   if (err) {
     console.log('Error: ', err);
   } else {
-    json = JSON.parse(data);
-    console.log(json);
+    artObj = JSON.parse(data);
   }
 });
 
+fs.readFile(dancefile, 'utf8', function(err, data){
+  if (err) {
+    console.log('Error: ', err);
+  } else {
+    danceObj = JSON.parse(data);
+  }
+});
+
+fs.readFile(musicfile, 'utf8', function(err, data){
+  if (err) {
+    console.log('Error: ', err);
+  } else {
+    musicObj = JSON.parse(data);
+  }
+});
+
+fs.readFile(theatrefile, 'utf8', function(err, data){
+  if (err) {
+    console.log('Error: ', err);
+  } else {
+    theatreObj = JSON.parse(data);
+  }
+});
 
 app.listen(3000, function() {
   console.log('Listening on port 3000...')
